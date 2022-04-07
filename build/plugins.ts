690063fec1f9b2cbad08169e61090307c8cd930d
freeroo2 <450543076@qq.com>
2022-04-07 14:19:34 +0800

    feat: 集成了vxe-table

--- build/plugins.ts
+++ build/plugins.ts
@@ -7,6 +7,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import WindiCSS from "vite-plugin-windicss";
 import { viteMockServe } from "vite-plugin-mock";
 import liveReload from "vite-plugin-live-reload";
+import styleImport from "vite-plugin-style-import";
 import VueI18n from "@intlify/vite-plugin-vue-i18n";
 import ElementPlus from "unplugin-element-plus/vite";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -91,6 +92,18 @@ export function getPluginsList(command, VITE_LEGACY) {
     }),
     // svg组件化支持
     svgLoader(),
+    // 按需加载vxe-table
+    styleImport({
+      libs: [
+        {
+          libraryName: "vxe-table",
+          esModule: true,
+          ensureStyleFile: true,
+          resolveComponent: name => `vxe-table/es/${name}`,
+          resolveStyle: name => `vxe-table/es/${name}/style.css`
+        }
+      ]
+    }),
     ElementPlus({}),
     // mock支持
     viteMockServe({
